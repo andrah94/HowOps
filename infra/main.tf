21cb3358d2a14bde64f9e9be8d618385f8a07ff2
andrah94 <andra.howard@ablethefoundation.org>
2025-06-22 19:04:41 -0700

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+  name    = "solopreneur-vpc"
+  cidr    = "10.0.0.0/16"
+}
